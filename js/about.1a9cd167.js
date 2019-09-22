93acbbd131ea8d4297f8fcf002c0e6fc9c5a7a31
Yaron de Leeuw <me@jarondl.net>
2019-09-22 08:39:08 -0400

    rebuild

--- js/about.1a9cd167.js
+++ js/about.1a9cd167.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("bike dock status")]),e._v("\n  This page shows status of bike docks by reading a gbfs file\n  on the client side.\n")])}],i=n("2877"),c={},o=Object(i["a"])(c,s,a,!1,null,null,null);t["default"]=o.exports}}]);
-//# sourceMappingURL=about.23b3f896.js.map
+//# sourceMappingURL=about.1a9cd167.js.map
